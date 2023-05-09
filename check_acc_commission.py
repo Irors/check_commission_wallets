899647d17ffda1430ac3f0e855ac7f7f2a824562
Irors <81914651+Irors@users.noreply.github.com>
2023-05-09 20:37:42 +0500

    Update check_acc_commission.py

--- check_acc_commission.py
+++ check_acc_commission.py
@@ -4,9 +4,10 @@ import requests
 import web3
 from web3 import Web3
 
+bnb = eth = ftm = arb = polygon = avax = 0
 
 with open("wallets.txt") as wallet:
-    wallets = wallet.readlines()[:-1]
+    wallets = wallet.readlines()
 
 with open("arb_api_key.txt") as arb_api:
     arb_key = arb_api.readline()
@@ -28,21 +29,43 @@ with open("polygon_api_key.txt") as polygon_api:
 
 
 for adresss in wallets:
-    bnb = eth = ftm = arb = opt = polygon = avax = 0
+    #bnb = eth = ftm = arb = polygon = avax = 0
 
-    response_bsc = requests.get(f"https://api.bscscan.com/api?module=account&action=txlist&address={adresss[:-1]}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={bsc_key}")
-    response_eth = requests.get(f"https://api.etherscan.com/api?module=account&action=txlist&address={adresss[:-1]}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={eth_key}")
-    response_ftm = requests.get(f"https://api.ftmscan.com/api?module=account&action=txlist&address={adresss[:-1]}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={ftm_key}")
-    response_arb = requests.get(f"https://api.arbiscan.io/api?module=account&action=txlist&address={adresss[:-1]}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={arb_key}")
-    response_polygon = requests.get(f"https://api.polygonscan.com/api?module=account&action=txlist&address={adresss[:-1]}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={polygon_key}")
-    response_avax = requests.get(f"https://api.snowtrace.io/api?module=account&action=txlist&address={adresss[:-1]}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={avax_key}")
+    if adresss == wallets[-1]:
+        response_bsc = requests.get(
+            f"https://api.bscscan.com/api?module=account&action=txlist&address={adresss}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={bsc_key}")
+        response_eth = requests.get(
+            f"https://api.etherscan.com/api?module=account&action=txlist&address={adresss}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={eth_key}")
+        response_ftm = requests.get(
+            f"https://api.ftmscan.com/api?module=account&action=txlist&address={adresss}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={ftm_key}")
+        response_arb = requests.get(
+            f"https://api.arbiscan.io/api?module=account&action=txlist&address={adresss}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={arb_key}")
+        response_polygon = requests.get(
+            f"https://api.polygonscan.com/api?module=account&action=txlist&address={adresss}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={polygon_key}")
+        response_avax = requests.get(
+            f"https://api.snowtrace.io/api?module=account&action=txlist&address={adresss}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={avax_key}")
 
-    print(f"\nAccount {adresss}\n")
+    else:
+        response_bsc = requests.get(
+            f"https://api.bscscan.com/api?module=account&action=txlist&address={adresss[:-1]}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={bsc_key}")
+        response_eth = requests.get(
+            f"https://api.etherscan.com/api?module=account&action=txlist&address={adresss[:-1]}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={eth_key}")
+        response_ftm = requests.get(
+            f"https://api.ftmscan.com/api?module=account&action=txlist&address={adresss[:-1]}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={ftm_key}")
+        response_arb = requests.get(
+            f"https://api.arbiscan.io/api?module=account&action=txlist&address={adresss[:-1]}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={arb_key}")
+        response_polygon = requests.get(
+            f"https://api.polygonscan.com/api?module=account&action=txlist&address={adresss[:-1]}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={polygon_key}")
+        response_avax = requests.get(
+            f"https://api.snowtrace.io/api?module=account&action=txlist&address={adresss[:-1]}&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc&apikey={avax_key}")
+
+
+    print(f"\nAccount {adresss}")
 
     ###
     try:
         for acc in response_bsc.json()['result']:
-            bnb += int(acc['gasUsed']) * int(acc['gasPrice']) / 10**18
+            bnb += int(acc['gasUsed']) * int(acc['gasPrice']) / 10**18 + int(acc['value']) / 10 ** 18
         print(bnb, "потрачено bnb")
     except:
         print("0 транзакций")
@@ -50,7 +73,7 @@ for adresss in wallets:
     ###
     try:
         for acc in response_eth.json()['result']:
-            eth += int(acc['gasPrice']) * int(acc['gasUsed']) / 10**18
+            eth = int(acc['gasPrice']) * int(acc['gasUsed']) / 10**18 + int(acc['value']) / 10 ** 18
         print(eth, "потрачено eth")
     except:
         print("0 транзакций")
@@ -58,7 +81,7 @@ for adresss in wallets:
     ###
     try:
         for acc in response_ftm.json()['result']:
-            ftm += int(acc['gasPrice']) * int(acc['gasUsed']) / 10**18
+            ftm = int(acc['gasPrice']) * int(acc['gasUsed']) / 10**18 + int(acc['value']) / 10 ** 18
         print(ftm, "потрачено ftm")
     except:
         print("0 транзакций")
@@ -67,7 +90,7 @@ for adresss in wallets:
     ###
     try:
         for acc in response_arb.json()['result']:
-            arb += int(acc['gasPriceBid']) * int(acc['gasUsed']) / 10 ** 18
+            arb = int(acc['gasPriceBid']) * int(acc['gasUsed']) / 10 ** 18 + int(acc['value']) / 10 ** 18
         print(arb, "потрачено arb_eth")
     except:
         print("0 транзакций")
@@ -76,7 +99,7 @@ for adresss in wallets:
     ###
     try:
         for acc in response_polygon.json()['result']:
-            polygon += int(acc['gasPrice']) * int(acc['gasUsed']) / 10**18
+            polygon = int(acc['gasPrice']) * int(acc['gasUsed']) / 10**18 + int(acc['value']) / 10 ** 18
         print(polygon, "потрачено matic")
     except:
         print("0 транзакций")
@@ -84,7 +107,8 @@ for adresss in wallets:
     ###
     try:
         for acc in response_avax.json()['result']:
-            avax += int(acc['gasPrice']) * int(acc['gasUsed']) / 10**18
+            avax = int(acc['gasPrice']) * int(acc['gasUsed']) / 10**18 + int(acc['value']) / 10 ** 18
         print(avax, "потрачено avax")
     except:
         print("0 транзакций")
+
